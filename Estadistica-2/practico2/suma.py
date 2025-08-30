171fae706ffe40ddb8edb52bbe3835a5f915840f
Sergio Cambara <scambaradorbigny@gmail.com>
2025-08-30 18:49:02 -0400

    Demostracion

---
+++ Estadistica-2/practico2/suma.py
@@ -0,0 +1,12 @@
+# Pedir al usuario dos números
+a = float(input("Ingrese el primer número: "))
+b = float(input("Ingrese el segundo número: "))
+
+# Realizar la suma
+resultado = a + b
+
+# Mostrar el resultado
+print("La suma de", a, "+", b, "es:", resultado)
+
+# correr en la terminal
+# python suma.py
